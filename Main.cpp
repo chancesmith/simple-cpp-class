36b2cb450ce99caf4d3293df30bf6ad9f94bf634
Chance Smith <chancesmithb@gmail.com>
2018-10-21 14:24:01 -0500

    📦  NEW: adds Log class and pontential methods

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,34 @@
 
 
+class Log
+{
+private:
+  /* data */
+public:
+  SetLevel(int level)
+  {
+  }
+  Warn(const char *message)
+  {
+  }
+  Log(/* args */);
+  ~Log();
+};
+
+Log::Log(/* args */)
+{
+}
+
+Log::~Log()
+{
+}
+
 int main()
 {
+  Log log;
+
+  log.SetLevel(LogLevelWarning);
+  log.warn("Hello!");
 
   return 0;
 }
